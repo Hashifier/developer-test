cdf38a1c69550076e26bcdb5911e45b7027b602c
Albin Falsen Lindqvist <albin.falsen@gmail.com>
2017-11-15 16:51:52 +0100

    Solution to proj0

--- proj0/fix_csv.py
+++ proj0/fix_csv.py
@@ -6,12 +6,16 @@ f = open(sys.argv[1])
 
 rows = csv.DictReader(f.readlines())
 
+fields = ['id','x','y','z']
+out = csv.DictWriter(sys.stdout, fieldnames=fields)
 seen = []
 for row in rows:
-    out = csv.DictWriter(sys.stdout, fieldnames=['id', 'x', 'y', 'z'])
+    id = row['id']
+    for field in fields[1:]:
+        row[field] = abs( int(row[field]) )
 
-    seen.append(row['id'])
-    if row['id'] in seen:
+    if id in seen:
+        seen.append(id)
         print("Found duplicate: {}".format(row['id']), file=sys.stderr)
         continue
 
